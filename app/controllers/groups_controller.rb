a4a351ab5014405be5b3fba2866634e9d9fd1797
nicko hsiao <nicko.hsiao@gmail.com>
2015-12-13 21:14:37 +0800

    github

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,23 @@
+class GroupsController < ApplicationController
+	def index
+		@groups = Group.all
+	end
+
+ 	def show
+ 	end
+
+ 	def new
+ 	end
+  
+ 	def edit
+ 	end
+
+ 	def create
+ 	end
+
+ 	def update
+ 	end
+
+	def destroy
+	end
+end
